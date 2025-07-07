47224c40dc810b7dc3c46cbfc2bc08847e30575d
Lucio <lucio.surcotaype@outlook.com>
2025-07-07 20:50:28 +0300

    map validation

--- src/map_validation.c
+++ src/map_validation.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:51:02 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/07/07 18:23:41 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/07/07 20:49:22 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,53 +26,43 @@ static int characters_to_check(char *line, const char *chars)
 	return (SUCCESS);
 }
 
-static int is_valid_walls(const char *path)
+static int is_valid_walls(const char *map)
 {
 	int counter;
 }
 
-static int has_valid_start_exit_collec(const char *path)
+static int has_valid_start_exit_collec(const char *map)
 {
 	int counter;
 }
-static int has_valid_chars(const char *path)
+static int has_valid_chars(const char **map)
 {
-	int fd;
-	char *chars;
 	char *line;
-	int	invalid_count;
-
+	char *chars;
+	int i;
+	
 	chars = "10CEP\n";
-	invalid_count = 0;
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return (FAIL);
-	line = get_next_line(fd);
-	while(line)
+	i = 0;
+	while(map[i])
 	{
-		if(characters_to_check(line, chars))
-			invalid_count++;
-		free(line);
-		line = get_next_line(fd);
+		if(characters_to_check(map[i], chars))
+			return (FAIL);
 	}
-	close(fd);
-	if(invalid_count > 0)
-		return (FAIL);
 	return (SUCCESS);
 }
-int		validate_map(char **map_path)
+int		validate_map(char **map)
 {
-	if (has_valid_chars(map_path))
+	if (has_valid_chars(map))
 	{
 		ft_putstr_fd(RED"Error\nInvalid characters!"RESET,2);
 		return (FAIL);
 	}
-	else if (has_valid_start_exit_collec(map_path))
+	else if (has_valid_start_exit_collec(map))
 	{
 		ft_putstr_fd(RED"Error\nMissing start, exit or collectible!"RESET,2);
 		return (FAIL);
 	}
-	else if (is_valid_walls(map_path))
+	else if (is_valid_walls(map))
 	{
 		ft_putstr_fd(RED"Error\nIncomplete walls!"RESET,2);
 		return (FAIL);
