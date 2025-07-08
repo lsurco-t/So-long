a1509475c66ad8f810f5b577543b8f1e5b0069d8
Lucio <lucio.surcotaype@outlook.com>
2025-07-08 12:55:12 +0300

    added check for valid path

--- so_long.h
+++ so_long.h
@@ -6,28 +6,28 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 10:23:16 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/07/07 23:13:38 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/07/08 12:38:49 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <string.h>
-# include <math.h>
-# include <errno.h>
-# include "./libft/libft.h"
 # include "./MLX42/include/MLX42/MLX42.h"
+# include "./libft/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 // Colors
-#define GREEN	"\033[0;32m"
-#define RED		"\033[0;31m"
-#define BLUE	"\033[0;34m"
-#define RESET 	"\033[0m"
+# define GREEN "\033[0;32m"
+# define RED "\033[0;31m"
+# define BLUE "\033[0;34m"
+# define RESET "\033[0m"
 
 // Window constants
 # define WIDTH 800
@@ -35,25 +35,34 @@
 # define TITLE "So Long"
 
 // Game elements
-# define WALL		'1'
-# define FLOOR		'0'
-# define COLLECTIBLE	'C'
-# define EXIT		'E'
-# define PLAYER		'P'
+# define WALL '1'
+# define FLOOR '0'
+# define COLLECTIBLE 'C'
+# define EXIT 'E'
+# define PLAYER 'P'
 
 // Key codes for MLX42
-# define KEY_W		MLX_KEY_W
-# define KEY_A		MLX_KEY_A
-# define KEY_S		MLX_KEY_S
-# define KEY_D		MLX_KEY_D
-# define KEY_ESC	MLX_KEY_ESCAPE
+# define KEY_W MLX_KEY_W
+# define KEY_A MLX_KEY_A
+# define KEY_S MLX_KEY_S
+# define KEY_D MLX_KEY_D
+# define KEY_ESC MLX_KEY_ESCAPE
 
 // RETURN CODE
-#define SUCCESS 0
-#define FAIL 1
+# define SUCCESS 0
+# define FAIL 1
 
 // Tile size in pixels
-# define TILE_SIZE	32
+# define TILE_SIZE 32
+
+typedef struct s_floofill
+{
+	char		**map;
+	int			x;
+	int			y;
+	int			*collec;
+	int			*exit;
+}				t_floodfill;
 
 // Game structure
 typedef struct s_game
@@ -68,29 +77,31 @@ typedef struct s_game
 	int			map_width;
 	int			map_height;
 	int			player_x;
+	int			player_y;
 	int			collectibles;
 	int			moves;
 	int			exit_open;
-}	t_game;
+}				t_game;
 
 // Map utils
 
-void	free_map_on_error(char **map, int lines);
-char	**parse_map(char *filename);
-void	free_map(char **map);
-int		get_rows(char **map);
-int		get_columns(char **map);
-int		characters_to_check(char *line, const char *chars);
+void			free_map_on_error(char **map, int lines);
+void			free_map(char **map);
+int				get_rows(char **map);
+int				get_columns(char **map);
+int				characters_to_check(char *line, const char *chars);
 
-int		validate_map(char **game);
+// Map Functions
+int				has_valid_path(char **map);
+int				validate_map(char **game);
+char			**parse_map(char *filename);
 
-
-int		init_game(t_game *game);
-int		init_images(t_game *game);
-void	render_map(t_game *game);
-void	move_player(t_game *game, int new_x);
-void	key_hook(mlx_key_data_t keydata, void *param);
-void	cleanup_game(t_game *game);
-void	ft_error(const char *message);
+int				init_game(t_game *game);
+int				init_images(t_game *game);
+void			render_map(t_game *game);
+void			move_player(t_game *game, int new_x);
+void			key_hook(mlx_key_data_t keydata, void *param);
+void			cleanup_game(t_game *game);
+void			ft_error(const char *message);
 
 #endif
--- src/map_valid_path.c
+++ src/map_valid_path.c
@@ -6,21 +6,138 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 11:30:53 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/07/08 11:33:48 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/07/08 12:54:26 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-static int floodfill()
+static char	**copy_map(char **map)
 {
-	
+	char	**copy;
+	int		rows;
+	int		i;
+
+	rows = get_rows(map);
+	copy = malloc((rows + 1) * sizeof(char *));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < rows)
+	{
+		copy[i] = ft_strdup(map[i]);
+		if (!copy[i])
+		{
+			free_map_on_error(copy, i);
+			return (NULL);
+		}
+		i++;
+	}
+	copy[rows] = NULL;
+	return (copy);
 }
+
+static void	get_player_pos(char **map, int *x, int *y)
+{
+	int	row;
+	int	col;
+
+	*x = -1;
+	*y = -1;
+	row = 0;
+	while (map[row])
+	{
+		col = 0;
+		while (map[row][col])
+		{
+			if (map[row][col] == 'P')
+			{
+				*x = col;
+				*y = row;
+				return ;
+			}
+			col++;
+		}
+		row++;
+	}
+}
+
+static int	count_collec(char **map)
+{
+	int	count;
+	int	row;
+	int	col;
+
+	count = 0;
+	row = 0;
+	while (map[row])
+	{
+		col = 0;
+		while (map[row][col])
+		{
+			if (map[row][col] == 'C')
+				count++;
+			col++;
+		}
+		row++;
+	}
+	return (count);
+}
+
+static void	floodfill(char **map, t_floodfill params)
+{
+	t_floodfill	next;
+
+	if (!map || !map[params.y] || !map[params.y][params.x])
+		return ;
+	if (map[params.y][params.x] == '1' || map[params.y][params.x] == 'V')
+		return ;
+	if (map[params.y][params.x] == 'C')
+		(*params.collec)++;
+	else if (map[params.y][params.x] == 'E')
+		*params.exit = 1;
+	map[params.y][params.x] = 'V';
+	next.collec = params.collec;
+	next.exit = params.exit;
+	next.x = params.x + 1;
+	next.y = params.y;
+	floodfill(map, next);
+	next.x = params.x - 1;
+	next.y = params.y;
+	floodfill(map, next);
+	next.x = params.x;
+	next.y = params.y + 1;
+	floodfill(map, next);
+	next.x = params.x;
+	next.y = params.y - 1;
+	floodfill(map, next);
+}
+
 int	has_valid_path(char **map)
 {
-	char **map_copy;
+	char		**map_copy;
+	int			total_collec;
+	int			found_collec;
+	int			found_exit;
+	t_floodfill	fill_params;
 
-	map_copy = map;
-	
+	found_collec = 0;
+	found_exit = 0;
+	map_copy = copy_map(map);
+	if (!map_copy)
+		return (FAIL);
+	get_player_pos(map_copy, &fill_params.x, &fill_params.y);
+	if (fill_params.x == -1 || fill_params.y == -1)
+	{
+		free_map(map_copy);
+		return (FAIL);
+	}
+	total_collec = count_collec(map_copy);
+	fill_params.collec = &found_collec;
+	fill_params.exit = &found_exit;
+	floodfill(map_copy, fill_params);
+	free_map(map_copy);
+	if (found_collec != total_collec || !found_exit)
+		return (FAIL);
 	return (SUCCESS);
 }
