6d1c0c8c141e4ce48895ed5e15a26daf29303cfe
Lucio <lucio.surcotaype@outlook.com>
2025-07-08 11:29:52 +0300

    game init  update

--- src/game_init.c
+++ src/game_init.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:51:19 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/07/07 22:23:33 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/07/08 11:26:14 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	init_game(t_game *game)
 {
+	game->map_width = get_columns(game->map);
+	game->map_height = get_rows(game->map);
+	game->mlx = mlx_init(game->map_width, game->map_height, TITLE, false);
 }
 
 int	init_images(t_game *game)
